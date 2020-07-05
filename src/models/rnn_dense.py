1842e103d495a16ff43a8060af7de3ad6e4506ed
Iliya Zhecev <iliya.zhechev@gmail.com>
2020-07-05 14:25:57 +0300

    Refactor model instantiation in all RNN based models

--- src/models/rnn_dense.py
+++ src/models/rnn_dense.py
@@ -15,7 +15,7 @@ class Model(RNNBase):
         self,
         num_precondition_frames,
         frame_size,
-        num_rnn_layers,
+        rnn_num_layers,
         num_actions,
         action_embedding_size,
         rnn_hidden_size,
@@ -24,7 +24,7 @@ class Model(RNNBase):
         super().__init__(
             action_embedding_size,
             rnn_hidden_size,
-            num_rnn_layers,
+            rnn_num_layers,
             recurrent_skip,
         )
 
@@ -33,16 +33,15 @@ class Model(RNNBase):
         self.frame_size = frame_size
         self.precondition_channels = num_precondition_frames * 3
         self.num_precondition_frames = num_precondition_frames
-        self.num_rnn_layers = num_rnn_layers
 
-        # self.direction_precondition = nn.Sequential(
-        #     tu.dense(i=1, o=128),
+        # self.precondition_encoder = nn.Sequential(
+        #     tu.dense(i=2, o=128),
         #     nn.BatchNorm1d(128),
         #     tu.dense(i=128, o=rnn_hidden_size),
         #     nn.BatchNorm1d(rnn_hidden_size),
         # )
 
-        self.frame_precondition = nn.Sequential(
+        self.precondition_encoder = nn.Sequential(
             tu.reshape(-1, self.precondition_channels * 32 * 32),
             tu.dense(i=self.precondition_channels * 32 * 32, o=128),
             nn.BatchNorm1d(128),
@@ -75,71 +74,36 @@ class Model(RNNBase):
         precondition = T.FloatTensor(precondition).to(self.device)
         actions = T.LongTensor(actions).to(self.device)
 
-        # If precondition with frames
-        if len(precondition.shape) == 5:  # (bs, num_frames, 3, H, W)
-            precondition_map = self.frame_precondition(precondition)
-        # else preconditioned with direction (from PONG)
-        else:
-            precondition.unsqueeze_(1)
-            precondition_map = self.direction_precondition(precondition)
-
+        precondition_map = self.precondition_encoder(precondition)
         action_vectors = self.action_embedding(actions)
-
         rnn_out_vectors, _ = self.rnn(action_vectors, precondition_map)
         frames = self.deconvolve_to_frame(rnn_out_vectors)
 
         return frames
 
-    def optim_step(self, batch):
-        actions = batch['actions']
-        observations = batch['observations']
-        terminals = batch['terminals']
 
-        if 'meta' in batch and 'direction' in batch['meta']:
-            precondition = batch['meta']['direction']
-        else:
-            precondition = observations[:, :self.num_precondition_frames]
-
-        actions = actions[:, self.num_precondition_frames - 1:-1]
-        observations = T.FloatTensor(observations).to(self.device)
-
-        terminals = T.BoolTensor(terminals).to(self.device)
-        terminals = terminals[:, self.num_precondition_frames:]
-
-        y_true = observations[:, self.num_precondition_frames:]
-
-        y_pred = self([actions, precondition, None])
-        y_pred = tu.mask_sequence(y_pred, ~terminals)
-
-        loss = F.binary_cross_entropy(y_pred, y_true)
-
-        if loss.requires_grad:
-            self.optim.zero_grad()
-            loss.backward()
-            self.optim.step()
-            self.scheduler.step()
-
-        return loss, {'y': y_true, 'y_pred': y_pred}
-
-
-def make_model(precondition_size, frame_size, num_actions):
+def make_model(config):
     return Model(
-        num_precondition_frames=precondition_size,
-        frame_size=frame_size,
-        num_rnn_layers=2,
-        num_actions=num_actions,
-        action_embedding_size=32,
-        rnn_hidden_size=32,
-        recurrent_skip=8,
+        num_precondition_frames=config['precondition_size'],
+        frame_size=config['frame_size'],
+        rnn_num_layers=config['rnn_num_layers'],
+        num_actions=config['num_actions'],
+        action_embedding_size=config['action_embedding_size'],
+        rnn_hidden_size=config['hidden_size'],
+        recurrent_skip=config['recurrent_skip'],
     )
 
 
 def sanity_check():
-    base_sanity_check(lambda: make_model(
+    base_sanity_check(lambda: make_model(dict(
         precondition_size=2,
         frame_size=(32, 32),
         num_actions=3,
-    ))
+        rnn_num_layers=2,
+        action_embedding_size=32,
+        hidden_size=32,
+        recurrent_skip=8,
+    )))
 
 
 if __name__ == '__main__':
--- src/models/rnn_spatial_asset_transformer.py
+++ src/models/rnn_spatial_asset_transformer.py
@@ -15,7 +15,7 @@ class Model(RNNBase):
         self,
         num_precondition_frames,
         frame_size,
-        num_rnn_layers,
+        rnn_num_layers,
         num_actions,
         action_embedding_size,
         rnn_hidden_size,
@@ -24,7 +24,7 @@ class Model(RNNBase):
         super().__init__(
             action_embedding_size,
             rnn_hidden_size,
-            num_rnn_layers,
+            rnn_num_layers,
             recurrent_skip,
         )
         self.name = 'RNN Spatial Asset Transformer'
@@ -32,9 +32,19 @@ class Model(RNNBase):
         self.frame_size = frame_size
         self.precondition_channels = num_precondition_frames * 3
         self.num_precondition_frames = num_precondition_frames
-        self.num_rnn_layers = num_rnn_layers
 
-        self.direction_precondition = nn.Sequential(
+        # self.precondition_encoder = nn.Sequential(
+        #     tu.cat_channels(),
+        #     tu.conv_to_flat(
+        #         input_size=frame_size,
+        #         channel_sizes=[self.precondition_channels, 16, 32, 64, 8],
+        #         ks=4,
+        #         s=1,
+        #         out_size=rnn_hidden_size,
+        #     ),
+        # )
+
+        self.precondition_encoder = nn.Sequential(
             tu.dense(i=2, o=128),
             nn.BatchNorm1d(128),
             tu.dense(i=128, o=rnn_hidden_size),
@@ -46,17 +56,6 @@ class Model(RNNBase):
         self.assets = nn.Parameter(
             T.randn(1, 1, self.num_assets, *self.frame_size[::-1]))
         self.assets.requires_grad = True
-
-        # self.frame_precondition = nn.Sequential(
-        #     tu.cat_channels(),
-        #     tu.conv_to_flat(
-        #         input_size=frame_size,
-        #         channel_sizes=[self.precondition_channels, 16, 32, 64, 8],
-        #         ks=4,
-        #         s=1,
-        #         out_size=rnn_hidden_size,
-        #     ),
-        # )
 
         self.action_embedding = nn.Embedding(
             num_embeddings=num_actions,
@@ -90,77 +89,47 @@ class Model(RNNBase):
         #     self.assets.data[0, 0, 1][:, -delim:] = f[:, -delim:]
         #     self.assets.data[0, 0, 2][:, delim:-delim] = f[:, delim:-delim]
 
-        # If precondition with frames
-        if len(precondition.shape) == 5:  # (bs, num_frames, 3, H, W)
-            precondition_map = self.frame_precondition(precondition)
-        # else preconditioned with direction (from PONG)
-        else:
-            precondition_map = self.direction_precondition(precondition)
+        precondition_map = self.precondition_encoder(precondition)
 
         action_vectors = self.action_embedding(actions)
         rnn_out_vectors, _ = self.rnn(action_vectors, precondition_map)
 
         assets = self.assets.repeat(bs, seq_len, 1, 1, 1)
         pred_frames = self.transform_frame([rnn_out_vectors, assets])
-        self.transformed_assets = pred_frames.clone()
+        self.transformed_assets = pred_frames
 
-        pred_frames = pred_frames.mean(dim=2, keepdim=True)
+        pred_frames = pred_frames.sum(dim=2, keepdim=True)
         pred_frames = T.sigmoid(pred_frames)
         pred_frames = pred_frames.repeat(1, 1, 3, 1, 1)
 
+        # mi, ma = pred_frames.min(), pred_frames.max()
+        # pred_frames = (pred_frames - mi) / (ma - mi)
+
         return pred_frames
 
-    def optim_step(self, batch):
-        actions = batch['actions']
-        observations = batch['observations']
-        terminals = batch['terminals']
 
-        if 'meta' in batch and 'direction' in batch['meta']:
-            precondition = batch['meta']['direction']
-        else:
-            precondition = observations[:, :self.num_precondition_frames]
-
-        actions = actions[:, self.num_precondition_frames - 1:-1]
-        input_frames = observations[:, self.num_precondition_frames - 1:-1]
-
-        terminals = T.BoolTensor(terminals).to(self.device)
-        terminals = terminals[:, self.num_precondition_frames:]
-
-        observations = T.FloatTensor(observations).to(self.device)
-        y_true = observations[:, self.num_precondition_frames:]
-
-        y_pred = self([actions, precondition, input_frames])
-        y_pred = tu.mask_sequence(y_pred, ~terminals)
-
-        loss = F.binary_cross_entropy(y_pred, y_true)
-
-        if loss.requires_grad:
-            self.optim.zero_grad()
-            loss.backward()
-            self.optim.step()
-            self.scheduler.step()
-
-        return loss, {'y': y_true, 'y_pred': y_pred}
-
-
-def make_model(precondition_size, frame_size, num_actions):
+def make_model(config):
     return Model(
-        num_precondition_frames=precondition_size,
-        frame_size=frame_size,
-        num_rnn_layers=2,
-        num_actions=num_actions,
-        action_embedding_size=16,
-        rnn_hidden_size=32,
-        recurrent_skip=4,
+        num_precondition_frames=config['precondition_size'],
+        frame_size=config['frame_size'],
+        rnn_num_layers=config['rnn_num_layers'],
+        num_actions=config['num_actions'],
+        action_embedding_size=config['action_embedding_size'],
+        rnn_hidden_size=config['hidden_size'],
+        recurrent_skip=config['recurrent_skip'],
     )
 
 
 def sanity_check():
-    base_sanity_check(lambda: make_model(
+    base_sanity_check(lambda: make_model(dict(
         precondition_size=2,
         frame_size=(32, 32),
         num_actions=3,
-    ))
+        rnn_num_layers=2,
+        action_embedding_size=16,
+        hidden_size=32,
+        recurrent_skip=4,
+    )))
 
 
 if __name__ == '__main__':
